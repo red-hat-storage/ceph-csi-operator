4432acc993082efe80412668be37f46452e7a557
Leela Venkaiah G <lgangava@ibm.com>
2025-03-07 11:42:58 +0000

    add tolerations to manager in only downstream build
    Signed-off-by: Leela Venkaiah G <lgangava@ibm.com>

--- Makefile.Downstream.mk
+++ Makefile.Downstream.mk
@@ -60,8 +60,11 @@ bundle: kustomize operator-sdk manifests
 	rm -rf bundle
 	rm -f config/manifests/kustomization.yaml
 	mkdir -p build dist
+	@# as per kustomize patch files should be in the folder and subfolders of kustomization.yaml for security reasons
+	cp config/manager/downstream_tolerations.yaml build/manager_downstream_tolerations.yaml
 	cd build && echo "$$BUILD_INSTALLER_OVERLAY" > kustomization.yaml
 	cd build && $(KUSTOMIZE) edit add resource ../config/default/
+	cd build && $(KUSTOMIZE) edit add patch --path manager_downstream_tolerations.yaml --kind Deployment --name controller-manager
 	cd config/manifests/bases && $(KUSTOMIZE) edit add annotation --force 'olm.skipRange':"$(SKIP_RANGE)"
 	cd config/manifests && $(KUSTOMIZE) create --resources bases,../../build
 	$(KUSTOMIZE) build config/manifests | $(OPERATOR_SDK) generate bundle \
